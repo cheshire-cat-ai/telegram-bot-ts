005d569ad6dfc34724921af8ac8966b20ee1d301
Dany <zdanymc@gmail.com>
2023-07-15 13:26:12 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,24 @@ bot.command('quit', async ctx => {
 
 bot.start(ctx => ctx.reply('Welcome dear user!'));
 
-bot.help(ctx => ctx.reply('Send me a sticker'));
+bot.help(ctx => ctx.reply('How can I help you?'));
 
 bot.on(message('text'), ctx => {
     cat.send(ctx.message.text);
     cat.onMessage(res => ctx.reply(res.content));
 });
 
-bot.launch();
+bot.launch().then(() => {
+    console.log(`Ready! Logged in as ${bot.botInfo?.username}`);
+}).catch(() => {
+    console.log('Unable to start the bot!');
+});
 
-console.log(`Ready! Logged in as ${bot.botInfo?.username}`);
+bot.catch(err => {
+    console.error('Error:', err)
+})
+
+console.log('Bot started!');
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
